6ab6957f204680100bc1ce4aeea2e4819f7d8b41
Shubham Shinde <shubham.shinde@settlin.in>
2022-07-17 21:32:45 +0530

    navbaradded

---
+++ salekart/src/App.js
@@ -0,0 +1,14 @@
+import Navbars from "./Navbar";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbars />
+      <h1> hi this is sale kart </h1>
+    </div>
+  );
+}
+
+export default App;
